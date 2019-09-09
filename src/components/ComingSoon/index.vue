2b87bc2611ab54bd1c73a420fdcac66308a54d78
unknown <fff>
2019-09-09 19:58:20 +0800

    add setData

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="movie_body">
 		<ul>
-			<li>
+			<!-- <li>
 				<div class="pic_show"><img src="/images/movie_1.jpg"></div>
 					<div class="info_list">
 							<h2>无名之辈</h2>
@@ -96,6 +96,18 @@
 					<div class="btn_pre">
 							预售
 					</div>
+				</li> -->
+                 <li v-for="item in comingList" :key="item.id">
+			<div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
+				<div class="info_list">
+						<h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png"> </h2>
+						<p><span class="person">{{item.wish}}</span> 人想看</p>
+						<p>主演:{{item.star}}</p>
+						<p>{{item.rt}}上映</p>
+					</div>
+					<div class="btn_pre">
+						预售
+					</div>
 				</li>
 			</ul>
 		</div>
@@ -103,7 +115,20 @@
 
 <script>
 export default {
-    name : 'ComingSoon'
+    name : 'ComingSoon',
+    data(){
+        return {
+            comingList : []
+        };
+    },
+    mounted() {
+        this.axios.get('/api/movieOnInfoList?cityId=10').then((res)=>{
+            var msg = res.msg;
+            if(msg === 'ok'){
+                this.comingList = res.data.data.comingList;
+            }
+        })
+    },
 }
 </script>
 
